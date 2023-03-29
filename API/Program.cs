515dd15153555e463bf953258b2e816cc6a70490
Iann221 <vincentiannugroho@gmail.com>
2023-03-29 16:45:28 +0700

    added security headers

--- API/Program.cs
+++ API/Program.cs
@@ -26,10 +26,31 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions(); //prevents mimesniffing
+app.UseReferrerPolicy(opt => opt.NoReferrer()); // untuk nambahin referrer policy
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny()); // prevents app being used inside iframe, prevent clickjacking
+app.UseCsp(opt => opt
+    .BlockAllMixedContent() // only load https content
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com")) // any stylesheets as long as it's from our domain are approved
+    .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+    .ScriptSources(s => s.Self())
+);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.Use(async (context, next) => {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy"); // cross origin resource sharing, bwt wharing sama yg diset di applicationServiceExtension
